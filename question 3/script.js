14e4ae2f4548918ed8f5ca52a320e3f855f165b2
sheetalsrivastavaa <95524734+sheetalsrivastavaa@users.noreply.github.com>
2021-12-08 21:16:40 +0530

    third commit

---
+++ question 3/script.js
@@ -0,0 +1,15 @@
+function funPink() {
+                    document.getElementById("text1").style.color ="pink";
+               }
+               function funBlue(){
+                   document.getElementById("text2").style.color="blue";
+               }
+               function funOrange(){
+                   document.getElementById("text3").style.color="orange";
+               }
+               function funYellow(){
+                   document.getElementById("text4").style.color="yellow";
+               }
+               function funGreen(){
+                   document.getElementById("text5").style.color="green";
+               }
